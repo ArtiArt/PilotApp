942adb6b6db9c857d1740b0597dce70930817932
Kowbix <106624245+Kowbix@users.noreply.github.com>
2022-11-14 23:08:59 +0100

    Add some filed

--- PilotAPP/app/src/main/java/com/example/pilotapp/SetRouteName.java
+++ PilotAPP/app/src/main/java/com/example/pilotapp/SetRouteName.java
@@ -4,10 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class SetRouteName extends AppCompatActivity {
+
+    EditText setNameTextView;
+    MaterialCardView startRecordingButton;
+    String routeName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +25,17 @@ public class SetRouteName extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_set_route_name);
+
+        setNameTextView = findViewById(R.id.setNameTV);
+        startRecordingButton = findViewById(R.id.startRecordingBtn);
+
+        startRecordingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                routeName = setNameTextView.getText().toString();
+
+            }
+        });
     }
 
     @Override
