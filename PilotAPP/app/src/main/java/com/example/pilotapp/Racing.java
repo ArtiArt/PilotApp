7bc0af168f248f51721f10979add9361f2dcc9e6
ArtiArt <artiartbacz@gmail.com>
2022-12-09 18:21:09 +0100

    commiting

--- PilotAPP/app/src/main/java/com/example/pilotapp/Racing.java
+++ PilotAPP/app/src/main/java/com/example/pilotapp/Racing.java
@@ -29,6 +29,7 @@ import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.card.MaterialCardView;
+import com.google.maps.android.SphericalUtil;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -36,12 +37,15 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Racing extends AppCompatActivity implements OnMapReadyCallback{
 
+    private String routName;
+    Context con = null;
     private boolean wasClicked=false;
     private MaterialCardView start_stopBtn;
     private TextView start_stop, routeDiscription;
@@ -51,8 +55,9 @@ public class Racing extends AppCompatActivity implements OnMapReadyCallback{
     LatLng[] routePoints;
     Polyline line;
     private static final String FILE_NAME = "/data/data/com.example.pilotapp/app_allRoutes/2.kml";
-    List<LatLng> filePoints;
-
+    List<LatLng> filePoints=null;
+    LatLng myLocation;
+    double distance, finalDistance;
 
 
     @Override
@@ -70,6 +75,9 @@ public class Racing extends AppCompatActivity implements OnMapReadyCallback{
         mapFragment.getMapAsync(this);
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
+        Intent intent = getIntent();
+        routName = intent.getStringExtra(SetRouteName.Extra_name);
+        con = getApplicationContext();
         routePoints = new LatLng[2];
         filePoints = new ArrayList<LatLng>();
         start_stopBtn.setOnClickListener(new View.OnClickListener(){
@@ -87,51 +95,7 @@ public class Racing extends AppCompatActivity implements OnMapReadyCallback{
                 {
                     //start recording time and change text to "Stop" and wasClicked on true
                     wasClicked=true;
-                   /* FileInputStream fis =null;
-                    String nextPoints="";
-                    try {
-
-                        fis = new FileInputStream(new File(FILE_NAME));
-                        InputStreamReader isr = new InputStreamReader(fis);
-                        BufferedReader br = new BufferedReader(isr);
-                        StringBuilder sb = new StringBuilder();
-                        String text;
-                        while ((text = br.readLine()) != null){
-                            sb.append(text).append("#\n");
-                        }
-                        nextPoints=sb.toString();
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    finally {
-                        if(fis != null){
-                            try{
-                                fis.close();
-                            }
-                            catch (IOException e){
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-
-                    String[] myArray=nextPoints.split("#");
-
-                    for(int i = 0; i<myArray.length;i++){
-
-                        if(myArray[i].contains("coordinates")){
-                            String temp =myArray[i].replaceFirst("<coordinates>","");
-                            temp=temp.replaceFirst("</coordinates>","");
-                            String[] cordin = temp.split(",");
-                            double lat = Double.parseDouble(cordin[0]);
-                            double lng = Double.parseDouble(cordin[1]);
-                            LatLng tempLatLng = new LatLng(lat,lng);
-                            filePoints.add(tempLatLng);
-                        }
-
-                    }
-                    routeDiscription.setText(Double.toString(filePoints.get(1).longitude));*/
+                   ReedFromKml();
                 }
             }
         });
@@ -173,28 +137,34 @@ public class Racing extends AppCompatActivity implements OnMapReadyCallback{
             public void onSuccess(Location location) {
 
                 LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                double first=52.22630132811354;
+                double second=21.027642057671056;
+                first=first+3;
+                second=second+3;
+                myLocation = new LatLng(first,second );
 
-                PolylineOptions pOptions = new PolylineOptions()
+
+                /*PolylineOptions pOptions = new PolylineOptions()
                         .width(25)
                         .color(Color.BLUE)
                         .geodesic(true);
-                    LatLng point = new LatLng(21.02538838686819,52.22821593405059 );
-                    //LatLng point1 = new LatLng(21.024978542191455,52.228057112498625 );
+                    //LatLng point = new LatLng(21.02538838686819,52.22821593405059 );
+                    LatLng point1 = new LatLng(52.22630132811354, 21.027642057671056 );
                     //LatLng point2 = new LatLng(21.029289869364764,52.22553392163475);
 
                 pOptions.add(latLng);
-                 pOptions.add(point);
+                 pOptions.add(point1);
 
                // pOptions.add(point1);
                  //pOptions.add(point2);
 
 
 
-                //myGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
-                //Polyline polyline = myGoogleMap.addPolyline(pOptions);
-                 //line = myGoogleMap.addPolyline(pOptions);
 
-                myGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
+                 line = myGoogleMap.addPolyline(pOptions);*/
+
+                myGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5));
             }
         });
     }
@@ -212,6 +182,80 @@ public class Racing extends AppCompatActivity implements OnMapReadyCallback{
 
             }
         }
+    }
+
+    private void ReedFromKml(){
+          FileInputStream fis =null;
+                    String nextPoints="";
+                    try {
+
+                        fis = new FileInputStream(new File(FILE_NAME));
+                        InputStreamReader isr = new InputStreamReader(fis);
+                        BufferedReader br = new BufferedReader(isr);
+                        StringBuilder sb = new StringBuilder();
+                        String text;
+                        while ((text = br.readLine()) != null){
+                            sb.append(text).append("#\n");
+                        }
+                        nextPoints=sb.toString();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    finally {
+                        if(fis != null){
+                            try{
+                                fis.close();
+                            }
+                            catch (IOException e){
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+
+                    String[] myArray=nextPoints.split("#");
+        double[] lat = {52.22630132811354,53.22630132811354,54.22630132811354,55.22630132811354,56.22630132811354};
+        double[] lng = {21.027642057671056,22.027642057671056,23.027642057671056,24.027642057671056,25.027642057671056};
+        int k=0;
+                    for(int i = 0; i<myArray.length;i++){
+
+                        if(myArray[i].contains("coordinates")){
+                            /*String temp =myArray[i].replaceFirst("<coordinates>","");
+                            temp=temp.replaceFirst("</coordinates>","");
+                            String[] cordin = temp.split(",");
+                            double lat = Double.parseDouble(cordin[0]);
+                            double lng = Double.parseDouble(cordin[1]);
+                            LatLng tempLatLng = new LatLng(lat,lng);*/
+
+
+
+                            LatLng latTemp = new LatLng(lat[k], lng[k]);
+                            filePoints.add(latTemp);
+                            k++;
+                        }
+                    }
+                    PolylineOptions pOptions = new PolylineOptions()
+                        .width(25)
+                        .color(Color.BLUE)
+                        .geodesic(true);
+                 for(int i =0; i<filePoints.size();i++){
+                     pOptions.add(filePoints.get(i));
+                 }
+        int p=0;
+                 for(int i =0; i<filePoints.size();i++){
+                     if(filePoints.get(i).latitude==myLocation.latitude && filePoints.get(i).longitude==myLocation.longitude){
+                         p=i;
+                     }
+                 }
+                 for(;p<filePoints.size();p++){
+                     if(p<filePoints.size()-1 ){
+                         distance = distance + SphericalUtil.computeDistanceBetween(filePoints.get(p), filePoints.get(p + 1));
+                         finalDistance = distance / 1000;
+                     }
+                 }
+                 line = myGoogleMap.addPolyline(pOptions);
+       routeDiscription.setText("Dystans: " + Math.round(finalDistance*100.0)/100.0 +" km");
     }
 
 }
